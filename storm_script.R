43094ca8e08df2d1773c4ef9ac0cdcd8636d570d
Andrew Pratt <apratt0303@gmail.com>
2017-08-10 09:32:00 -0400

    Small addition to storm_script.R
    Added a file.exists() statement at beginning of document

--- storm_script.R
+++ storm_script.R
@@ -5,7 +5,9 @@
 
 # Download the .csv.bz2 file
 url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
-download.file(url, destfile = "stormData.csv.bz2")
+if(!file.exists("stormData.csv.bz2")){
+        download.file(url, destfile = "stormData.csv.bz2")
+}
 
 # Unzip the bz2 file, store it in a temporary file, read in
 # data using fread, and finally delete the temporary file.
